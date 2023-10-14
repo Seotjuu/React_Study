68457b3e6c612f3a3bb454c3421d536816b28068
chanyoungo <142957021+chanyoungo@users.noreply.github.com>
2023-10-14 12:04:48 +0900

    [feat] Proejct Start   - 불필요 파일 제거   - 컴포넌트 구분   - gitingnore 설정   - readme 간단하게 설정

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      test file
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/page/userLogin.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react"
+
+const userLogin = () => {
+
+    return(
+        <div></div>
+    )
+}
+
+export default userLogin;
